4d66df07abca19456a2900d0db09018d55a6b48d
Marcio Ponte <mjmponte@gmail.com>
2022-08-04 10:42:14 -0300

    Projeto criado

---
+++ src/test/java/com/example/testeSpringBootJPAHibernate/TesteSpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeSpringBootJPAHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
